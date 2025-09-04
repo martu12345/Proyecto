4d01b6b81f69f4f72d9db7f8daf97d2aa735e741
sopitaxd <bakabakadance@gmail.com>
2025-09-04 00:43:59 -0300

    cambios en modelo

--- apps/Modelos/modeloUsuario.php
+++ apps/Modelos/modeloUsuario.php
@@ -4,11 +4,39 @@ class Usuario {
     private $email;
     private $contraseña;
     private $telefono;
-    public function __construct($idUsuario, $email, $contraseña, $telefono)
-// los if son las rne
+    public function __construct($idUsuario, $email, $contraseña, $telefono) {
+// los if son las rne (hay que hacerlas)
     $this->idUsuario = $idUsuario;
     $this->email = $email;
-    $this->contraseña = $contraseña;
+    $this->contraseña = password_hash($contraseña, PASSWORD_DEFAULT);
+    $this->telefono = $telefono;
+    }
+        // getters
+    public function getIdUsuario() {
+         return $this->idUsuario;
+         }
+    public function getEmail() {
+         return $this->email;
+         }
+    public function getTelefono() {
+         return $this->telefono;
+         }
 
+    // setters
+    public function setEmail($email) {
+         $this->email = $email;
+         }
+    public function setContraseña($contraseña) { 
+
+        $this->contraseña = password_hash($contraseña, PASSWORD_DEFAULT);
+         // dios sabra si esto esta bien
+    }
+    public function setTelefono($telefono) { 
+        $this->telefono = $telefono; 
+    }
+    public function verificarContraseña($contraseñaIngresada) {
+        return password_verify($contraseñaIngresada, $this->contraseña);
+// esto tampoco se si esta bien
+}
 }
 ?>
