3e007b19f480b2c78a9203c61168a1a252de0179
Martu <yomartinasofia@gmail.com>
2025-09-07 15:24:00 -0300

    pepapig

--- apps/Modelos/conexion.php
+++ apps/Modelos/conexion.php
@@ -2,7 +2,7 @@
 $host = "localhost";       
 $db   = "MiProyecto";    
 $user = "root";         
-$pass = "M4rtina2007.";      
+$pass = "pepapig";      
 
 
 $conn = new mysqli($host, $user, $pass, $db);
@@ -11,17 +11,6 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-
-// prueba para ver si fucona la base de datos
-$sql = "INSERT INTO Usuario (email, contraseña) VALUES ('martina@example.com', '123456')";
-
-
-if ($conn->query($sql) === TRUE) {
-    echo "Registro insertado correctamente 😎";
-} else {
-    echo "Error al insertar: " . $conn->error;
-}
-
 
 
 ?>
