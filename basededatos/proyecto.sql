59ec24a7a9a135d0d57b1e1f146037a4302af00e
Martu <yomartinasofia@gmail.com>
2025-10-17 18:07:16 -0300

    Base final

--- basededatos/proyecto.sql
+++ basededatos/proyecto.sql
@@ -23,23 +23,14 @@ DROP TABLE IF EXISTS `administra`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `administra` (
-  `IdServicio` int NOT NULL,
-  `IdUsuario` int NOT NULL,
+  `idAdministra` int NOT NULL AUTO_INCREMENT,
+  `idUsuario` int NOT NULL,
+  `idServicio` int NOT NULL,
   `fecha` datetime NOT NULL,
-  PRIMARY KEY (`IdServicio`,`IdUsuario`),
-  KEY `fk_administra_admin` (`IdUsuario`),
-  CONSTRAINT `fk_administra_servicio` FOREIGN KEY (`IdServicio`) REFERENCES `servicio` (`IdServicio`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`idAdministra`),
+  UNIQUE KEY `idx_servicio_usuario` (`idServicio`,`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `administra`
---
-
-LOCK TABLES `administra` WRITE;
-/*!40000 ALTER TABLE `administra` DISABLE KEYS */;
-/*!40000 ALTER TABLE `administra` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `administrador`
@@ -59,16 +50,6 @@ CREATE TABLE `administrador` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `administrador`
---
-
-LOCK TABLES `administrador` WRITE;
-/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
-INSERT INTO `administrador` VALUES (14,12);
-/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `brinda`
 --
 
@@ -78,23 +59,12 @@ DROP TABLE IF EXISTS `brinda`;
 CREATE TABLE `brinda` (
   `IdUsuario` int NOT NULL,
   `IdServicio` int NOT NULL,
-  `notificacion` enum('no leido','leido') DEFAULT NULL,
   PRIMARY KEY (`IdUsuario`,`IdServicio`),
   KEY `IdServicio` (`IdServicio`),
   CONSTRAINT `brinda_ibfk_1` FOREIGN KEY (`IdUsuario`) REFERENCES `empresa` (`IdUsuario`) ON DELETE CASCADE,
   CONSTRAINT `brinda_ibfk_2` FOREIGN KEY (`IdServicio`) REFERENCES `servicio` (`IdServicio`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `brinda`
---
-
-LOCK TABLES `brinda` WRITE;
-/*!40000 ALTER TABLE `brinda` DISABLE KEYS */;
-INSERT INTO `brinda` VALUES (4,5,NULL),(4,6,NULL),(6,7,NULL),(6,8,NULL),(6,9,NULL),(6,10,NULL),(6,11,NULL),(7,12,NULL),(7,13,NULL),(10,14,NULL),(10,17,NULL),(10,18,NULL);
-/*!40000 ALTER TABLE `brinda` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `cliente`
@@ -112,16 +82,6 @@ CREATE TABLE `cliente` (
   CONSTRAINT `fk_cliente_usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cliente`
---
-
-LOCK TABLES `cliente` WRITE;
-/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'Martolis','Ferreira',NULL),(9,'Martina','Pol','/Proyecto/public/imagen/clientes/68d9c846290ac_Captura de pantalla 2024-09-11 210437.png'),(11,'PaolA','ddeede','/Proyecto/public/imagen/clientes/68ee497d9e39a_2.png');
-/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `comunica`
@@ -149,18 +109,8 @@ CREATE TABLE `comunica` (
   CONSTRAINT `fk_comunica_emisor` FOREIGN KEY (`idUsuarioEmisor`) REFERENCES `usuario` (`IdUsuario`),
   CONSTRAINT `fk_comunica_empresa` FOREIGN KEY (`IdUsuarioEmpresa`) REFERENCES `empresa` (`IdUsuario`) ON DELETE CASCADE,
   CONSTRAINT `fk_mensaje_respuesta` FOREIGN KEY (`IdMensajePadre`) REFERENCES `comunica` (`IdMensaje`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comunica`
---
-
-LOCK TABLES `comunica` WRITE;
-/*!40000 ALTER TABLE `comunica` DISABLE KEYS */;
-INSERT INTO `comunica` VALUES (1,11,10,'ODIO A SOFIA','La odio ','2025-10-03 14:30:39',1,'leído',NULL),(2,11,4,'Odi ','HOLIS','2025-10-03 22:06:06',1,'leído',NULL),(3,11,10,'Rechazo de reserva','Eres horrible','2025-10-04 15:25:50',10,'leído',NULL),(4,11,10,'Rechazo de reserva','No podemos','2025-10-04 15:34:19',10,'leído',NULL),(5,11,10,'Rechazo de reserva','Lo odio','2025-10-04 15:36:29',10,'leído',NULL),(6,11,10,'Rechazo de reserva','No me gustan tus dientes','2025-10-05 03:03:01',10,'leído',NULL),(7,11,7,'hola jorge','como estas','2025-10-07 14:22:43',11,'leído',NULL),(8,11,10,'Cafe','Me gusta el cafe ','2025-10-10 21:18:35',11,'leído',NULL),(9,11,10,'popa','dedwrehdfn','2025-10-10 22:28:39',11,'leído',NULL),(11,11,10,'pORQUE','SE ROMPIO TODO ','2025-10-11 13:35:57',11,'leído',NULL),(12,11,10,'pORQUE','SE ROMPIO TODO ','2025-10-11 13:37:37',11,'leído',NULL),(16,11,10,'OJALA QUE FUNCIONE','Funcioa','2025-10-14 02:43:55',11,'leído',NULL),(17,11,10,'NULA','nOTIFICIOM','2025-10-14 02:50:13',11,'leído',NULL),(18,11,10,'Re: NULA','Holis','2025-10-14 04:20:41',10,'leído',17),(19,11,10,'Re: Re: NULA','OKIS','2025-10-14 04:43:41',11,'leído',18),(20,11,10,'Re: Re: NULA','nb','2025-10-14 04:44:12',11,'leído',18),(21,11,10,'Re: Re: NULA','nb','2025-10-14 04:44:14',11,'leído',18),(22,11,10,'Re: Rechazo de reserva','okiw','2025-10-14 04:44:39',11,'leído',6),(23,11,10,'Re: Rechazo de reserva','Problema tuyo papi','2025-10-14 04:46:29',11,'leído',6),(24,11,10,'Re: Rechazo de reserva','Problema tuyo papi','2025-10-14 04:46:53',11,'leído',6),(25,11,10,'Re: Re: Rechazo de reserva','la vaca lola','2025-10-14 04:52:38',10,'leído',24),(26,11,10,'Re: Re: Rechazo de reserva','Vaca lola','2025-10-14 04:53:38',10,'leído',24),(27,11,10,'Re: Re: Rechazo de reserva','noololo','2025-10-14 04:54:14',10,'leído',22),(28,11,10,'Re: Re: Re: Rechazo de reserva','que te pasa che','2025-10-14 05:03:34',11,'leído',27),(29,11,10,'Re: Re: Re: Re: Rechazo de reserva','Me pasa de todo wacho ','2025-10-14 05:10:44',10,'leído',28),(30,11,10,'Re: Re: Re: Re: Rechazo de reserva','oka','2025-10-14 05:17:23',10,'leído',28),(31,11,10,'Re: Re: Re: Re: Re: Rechazo de reserva','que te haces wacho ','2025-10-14 05:29:37',11,'leído',30);
-/*!40000 ALTER TABLE `comunica` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `contrata`
@@ -185,18 +135,8 @@ CREATE TABLE `contrata` (
   CONSTRAINT `fk_contrata_cliente` FOREIGN KEY (`IdUsuario`) REFERENCES `cliente` (`IdUsuario`) ON DELETE CASCADE,
   CONSTRAINT `fk_contrata_servicio` FOREIGN KEY (`IdServicio`) REFERENCES `servicio` (`IdServicio`) ON DELETE CASCADE,
   CONSTRAINT `chk_calificacion` CHECK (((`Calificacion` between 1 and 5) or (`Calificacion` is null)))
-) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `contrata`
---
-
-LOCK TABLES `contrata` WRITE;
-/*!40000 ALTER TABLE `contrata` DISABLE KEYS */;
-INSERT INTO `contrata` VALUES (2,11,14,'2025-10-09','03:44:00',2,NULL,'Cancelado','leido'),(5,11,14,'2025-10-02','04:07:00',3,'NAAAA','Finalizado','leido'),(6,11,14,'2025-10-02','06:50:00',4,'ameee','Finalizado','leido'),(7,11,14,'2025-10-02','10:20:00',2,'DEEAA','Finalizado','leido'),(8,11,14,'2025-10-03','03:00:00',3,'YES','Finalizado','leido'),(9,11,14,'2025-10-03','06:20:00',5,'OKIS','Finalizado','leido'),(10,11,14,'2025-10-03','08:20:00',1,'labubu','Finalizado','leido'),(11,11,14,'2025-10-03','10:30:00',2,NULL,'Finalizado','leido'),(12,11,14,'2025-10-03','12:40:00',2,NULL,'Finalizado','leido'),(13,11,14,'2025-10-03','14:50:00',2,NULL,'Finalizado','leido'),(16,11,14,'2025-10-05','12:00:00',2,NULL,'Cancelado','leido'),(17,11,14,'2025-10-10','11:00:00',2,NULL,'Finalizado','leido'),(18,11,14,'2025-10-24','11:00:00',2,NULL,'Cancelado','leido'),(19,11,5,'2025-10-03','11:00:00',2,NULL,'Cancelado','no_leido'),(20,11,5,'2025-10-10','11:00:00',2,NULL,'Pendiente','no_leido'),(21,11,8,'2025-10-09','12:22:00',2,NULL,'Cancelado','no_leido'),(22,11,5,'2025-10-05','03:00:00',2,NULL,'Cancelado','no_leido'),(23,11,5,'2025-10-09','23:33:00',2,NULL,'Pendiente','no_leido'),(31,11,14,'2025-10-17','11:00:00',2,NULL,'En proceso','leido'),(32,11,14,'2025-10-09','12:00:00',2,NULL,'Cancelado','leido'),(33,11,14,'2025-10-19','12:00:00',2,NULL,'Cancelado','leido'),(34,11,14,'2025-10-11','11:00:00',2,NULL,'Cancelado','leido'),(35,11,14,'2025-10-24','20:40:00',2,NULL,'En proceso','leido'),(36,11,14,'2025-10-05','09:20:00',2,NULL,'Cancelado','leido'),(37,11,17,'2025-10-04','10:57:00',2,NULL,'Finalizado','leido'),(38,11,17,'2025-10-04','18:26:00',2,NULL,'Finalizado','leido'),(39,11,17,'2025-10-03','10:00:00',5,'Excelente servicio, muy amable y rápido.','Finalizado','leido'),(40,11,17,'2025-10-03','14:30:00',4,'Buen servicio, pero tardaron un poco más de lo esperado.','Finalizado','leido'),(41,11,14,'2025-10-16','11:00:00',2,NULL,'Pendiente','leido'),(42,11,18,'2025-10-04','11:00:00',2,NULL,'Cancelado','leido'),(44,11,18,'2025-10-05','11:00:00',2,NULL,'Cancelado','leido'),(45,11,18,'2025-10-05','03:40:00',2,NULL,'Cancelado','leido'),(48,11,14,'2025-10-23','02:00:00',2,NULL,'Pendiente','leido'),(58,11,14,'2025-10-18','12:00:00',2,NULL,'Pendiente','leido'),(65,11,14,'2025-10-19','09:20:00',2,NULL,'Pendiente','leido'),(66,11,14,'2025-10-18','11:00:00',2,NULL,'Pendiente','leido'),(67,11,14,'2025-10-18','10:00:00',2,NULL,'Pendiente','leido'),(68,11,18,'2025-10-10','11:00:00',2,NULL,'Pendiente','leido'),(69,11,18,'2025-10-10','10:00:00',2,NULL,'Pendiente','leido'),(70,11,18,'2025-10-10','08:00:00',2,NULL,'Pendiente','leido'),(71,11,18,'2025-10-10','07:00:00',2,NULL,'Pendiente','leido'),(72,11,18,'2025-10-10','06:00:00',2,NULL,'Pendiente','leido'),(73,11,18,'2025-10-10','05:00:00',2,NULL,'En proceso','leido'),(74,11,18,'2025-10-10','04:00:00',2,NULL,'En proceso','leido'),(75,11,18,'2025-10-10','03:00:00',2,NULL,'En proceso','leido'),(76,11,18,'2025-10-10','02:00:00',2,NULL,'En proceso','leido'),(77,11,14,'2025-10-24','02:00:00',2,NULL,'Pendiente','leido'),(78,11,14,'2025-10-11','21:00:00',2,NULL,'Pendiente','leido'),(79,11,14,'2025-10-19','02:30:00',2,NULL,'Pendiente',NULL),(80,11,14,'2025-10-14','03:30:00',2,NULL,'Pendiente','leido'),(81,11,14,'2025-10-11','02:00:00',2,NULL,'Pendiente','leido'),(82,11,2,'2025-10-09','20:32:00',2,NULL,'Pendiente','no_leido'),(83,11,14,'2025-10-30','02:59:00',NULL,NULL,'Finalizado','leido'),(84,11,14,'2025-11-01','11:00:00',NULL,NULL,'Pendiente','leido'),(85,11,14,'2025-10-31','02:40:00',NULL,NULL,'Pendiente','leido'),(86,11,14,'2025-10-16','08:59:00',NULL,NULL,'Pendiente','leido');
-/*!40000 ALTER TABLE `contrata` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `denuncia`
@@ -217,18 +157,8 @@ CREATE TABLE `denuncia` (
   KEY `idEmpresa` (`idEmpresa`),
   CONSTRAINT `denuncia_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`IdUsuario`),
   CONSTRAINT `denuncia_ibfk_2` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`IdUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `denuncia`
---
-
-LOCK TABLES `denuncia` WRITE;
-/*!40000 ALTER TABLE `denuncia` DISABLE KEYS */;
-INSERT INTO `denuncia` VALUES (1,11,10,'DenunciarEmpresa','2025-10-12 00:18:01','ODIO'),(2,11,10,'DenunciarEmpresa','2025-10-12 00:20:37','No me gusto'),(3,11,10,'DenunciarEmpresa','2025-10-12 03:59:01','Por'),(4,11,10,'DenunciarEmpresa','2025-10-12 04:05:42','Holis'),(5,11,10,'DenunciarEmpresa','2025-10-13 10:46:39','Horrible\r\n'),(6,11,10,'DenunciarEmpresa','2025-10-13 10:49:11','NOS'),(7,11,10,'DenunciarEmpresa','2025-10-13 10:49:50','LOL'),(8,11,10,'DenunciarEmpresa','2025-10-13 10:51:03','XD'),(9,11,10,'DenunciarServicio','2025-10-13 10:53:29','ARROZ\r\n'),(10,11,10,'DenunciarServicio','2025-10-14 04:23:22','Horirbrle no me guso profis');
-/*!40000 ALTER TABLE `denuncia` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `empresa`
@@ -249,16 +179,6 @@ CREATE TABLE `empresa` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `empresa`
---
-
-LOCK TABLES `empresa` WRITE;
-/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
-INSERT INTO `empresa` VALUES (2,'Manitas','Boulevard','1103','empresa_2.jpg'),(3,'MartinaF','Peña','2003',NULL),(4,'Manitass','Peña','1120','empresa_4.jpg'),(6,'Jorge','Peña','3904',NULL),(7,'AlejandraS','Peña','3904','empresa_7.png'),(8,'Pepe','Perez','1120',NULL),(10,'Eduar','deded','1222','empresa_10.png');
-/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `propietario`
 --
 
@@ -271,16 +191,6 @@ CREATE TABLE `propietario` (
   CONSTRAINT `propietario_ibfk_1` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `propietario`
---
-
-LOCK TABLES `propietario` WRITE;
-/*!40000 ALTER TABLE `propietario` DISABLE KEYS */;
-INSERT INTO `propietario` VALUES (12);
-/*!40000 ALTER TABLE `propietario` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `servicio`
@@ -300,18 +210,8 @@ CREATE TABLE `servicio` (
   `Duracion` decimal(3,1) NOT NULL,
   PRIMARY KEY (`IdServicio`),
   CONSTRAINT `chk_precio` CHECK ((`Precio` >= 0))
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `servicio`
---
-
-LOCK TABLES `servicio` WRITE;
-/*!40000 ALTER TABLE `servicio` DISABLE KEYS */;
-INSERT INTO `servicio` VALUES (2,'Maquillaje a domicilio ','Belleza','Hacemos maquillaje para todo el mundo',100,'1758148751','Cerro Largo',1.0),(3,'Correcion de maquillaje','Belleza','Coreccion de tu maquillaje para que quedes perfecta',1200,'1758148788_Captura de pantalla 2024-09-21 235022.png','Montevideo',1.0),(5,'Arreglos de  AUTOS','Autos','Arreglamos autitos',1450,'1758318469_Captura de pantalla 2024-09-16 225841.png','Artigas',1.0),(6,'cocinando con marta','Cocina','Cocicnamos jutnos ',170,'1758321129_Captura de pantalla 2024-09-16 225841.png','Soriano',1.0),(7,'Cocina fria ','Cocina','Cocinamso cocina fria peor no limpaimos ',1500,'1758368184_Captura de pantalla 2024-09-11 210437.png','Florida',1.0),(8,'Arreglos de  autos','Autos','Arreglamos autitos',1455,'1758373882_Captura de pantalla 2024-09-11 210437.png','Canelones',1.0),(9,'Hacemos pepas para comer ','Cocina','Hacemos ppeitas para comer ',12,'1758373959_Captura de pantalla 2024-09-23 220654.png','Artigas',1.0),(10,'reparaciones de ruedas','Autos','Reparamos tus ruedas',135000,'1758375851_Captura de pantalla 2024-09-22 004915.png','Canelones',1.0),(11,'All face ','Belleza','Maqui para todos ',123,'1758375936_Captura de pantalla 2024-09-23 223516.png','Soriano',1.0),(12,'PApaS','Cocina','Hacemos papas fritas para todos ',1202,'1759083199_Captura de pantalla 2024-09-11 210437.png','San José',1.0),(13,'JorgeS','Educación','MUerte a las papas',458888,'1758977739_Captura de pantalla 2024-10-20 140811.png','Durazno',1.0),(14,'Duracion','Transporte','Hacemos donas',23,'1760214894_dona.jpg','Canelones',2.0),(17,'Prueba de finalizacion','Transporte','Hacemos no se que ',390,'68ea653a2f7fd_enix.png','Salto',1.0),(18,'Papas a la parmesana CON CREMA','Arte y Cultura','hacemos papas ala francesa muy ricas. a no eran a la crema lolsiento. :(',123,'68ea651a48803_PAPAS.png','Soriano',2.0);
-/*!40000 ALTER TABLE `servicio` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `telefono`
@@ -330,16 +230,6 @@ CREATE TABLE `telefono` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `telefono`
---
-
-LOCK TABLES `telefono` WRITE;
-/*!40000 ALTER TABLE `telefono` DISABLE KEYS */;
-INSERT INTO `telefono` VALUES (1,'092898383'),(2,'098888787'),(3,'091888989'),(4,'098777666'),(6,'098765567'),(7,'094555444'),(8,'098999222'),(9,'096543454'),(10,'098765454'),(11,'097666555'),(12,'099123456'),(14,'092383999');
-/*!40000 ALTER TABLE `telefono` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `usuario`
 --
 
@@ -352,18 +242,8 @@ CREATE TABLE `usuario` (
   `contraseña` varchar(255) NOT NULL,
   PRIMARY KEY (`IdUsuario`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'yomartinasofia@gmail.com','$2y$10$Fo0tGn9lkN/A4m4EmbmazeP5uT5a2URp2K451BfAQs./6V0qS1mL.'),(2,'manitasss@gmail.com','$2y$10$kH5SZeLpUrPMoXD8HCRAI.sZflfyV1rNpoWcvYnrf.y9gO4VVMENy'),(3,'yomar@gmail.com','$2y$10$RJ2SeyVn4i/U6McZe/mSwO13z9d7j5pkdvyyJkMuxUipHXSrR3vR2'),(4,'manitass@gmail.com','$2y$10$nKaRDHQiGsqyioUYB7ccCuwaTa2I3Gjj68q9i9F0zi.Oytlm2qJGe'),(6,'sofi@gmail.com','$2y$10$jZeuvUfx.jKqSFSsDxcZ/.1NFuyV5UxZbFaA9X.PkG4Y6M0YrZBPG'),(7,'alejandra@gmail.com','$2y$10$VmZj2HY6NKCOyQnh.yfQ0.4r9vqYwf3QvkeI4sybyQEjp91ci0PrO'),(8,'pepe@gmail.com','$2y$10$J2mNW6mRxRK/Fxyae5ajbOjLbKhABSbbKzaeY3BvKv16SuRSsTEg6'),(9,'pepa@gmail.com','$2y$10$LyBQto.dv3OjiLGS1u3LTOcmS3PqwUCPrDq8lXV0fUdrlKh7pBBX6'),(10,'eduar@gmail.com','$2y$10$.05JXUo38YrzAl8jxEnrUerTn98qCBAAaYyxQO24AG7ByzyScAle.'),(11,'mateo@gmail.com','$2y$10$tsffOOE/q2kIcAexeF32uO5fPhv0KXe3arQZSWWPVRr82wPMLTmae'),(12,'pruebapropietario2@hotmail.com','$2y$10$Ls45SZ9RtSZdWCzuvetP4uuAt00o4hDdoQxArzs718qKMnkpIj8zG'),(14,'rocco@gmail.com','$2y$10$O9pQzxZDuTt7xGmWxtajH.A/8RQRQCD8a3xbcJcw0XCR9UpJyhpAm');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -374,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-14 11:35:44
+-- Dump completed on 2025-10-17 18:07:03
